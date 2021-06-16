bb29b16f720416023a468c51d563b6da76de2b61
aldoecheverria <aldoecheverria@yahoo.com.mx>
2021-06-16 18:05:24 -0500

    header part 1 ready

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,36 @@
 import React from 'react'
 import './Header.css';
+import MenuIcon from '@material-ui/icons/Menu'
+import { IconButton, Avatar } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search'
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import AppsIcon from '@material-ui/icons/Apps';
+import NotificationsIcon from '@material-ui/icons/Notifications'
+
 
 function Header() {
     return (
         <div className="header">
-            <h1>This is a header</h1>
+            <div className="header__left">
+                <IconButton>
+                <MenuIcon/>
+                </IconButton>
+                <img src="http://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png" alt='' />
+            </div>
+            <div className="header__middle">
+                <SearchIcon/>
+                <input placeholder='Busca algun correo' type="text"/>
+                <ArrowDropDownIcon className="header__inputCaret"/>
+            </div>
+            <div className="header__right">
+            <IconButton>
+            <AppsIcon/>
+          </IconButton>
+          <IconButton>
+            <NotificationsIcon/>
+          </IconButton>
+          <Avatar/>
+            </div>
         </div>
     )
 }
