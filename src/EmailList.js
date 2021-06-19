45bb352584ff6a54738422ce6b196c36794622fa
aldoecheverria <aldoecheverria@yahoo.com.mx>
2021-06-18 20:46:24 -0500

    all icons from emailList are done

--- src/EmailList.js
+++ src/EmailList.js
@@ -1,10 +1,49 @@
-import React from 'react'
+import React from 'react';
+import ArrowDropDown from '@material-ui/icons/ArrowDropDown';
+import RedoIcon from "@material-ui/icons/Redo";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { Checkbox, IconButton} from "@material-ui/core";
+import SettingsIcon from "@material-ui/icons/Settings";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import KeyboardHideIcon from "@material-ui/icons/KeyboardHide"
 import './EmailList.css'
+
 
 function EmailList() {
     return (
         <div className='emailList'>
-            
+            <div className="emailList__settings">
+                <div className="emailList__settingsLeft">
+                    <Checkbox/>
+                    <IconButton>
+                        <ArrowDropDown/>
+                    </IconButton>
+                    <IconButton>
+                        <RedoIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon/>
+                    </IconButton>
+                </div>
+                <div className="emailList__settingsRight">
+                    <IconButton>
+                        <ChevronLeftIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <ChevronRightIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <KeyboardHideIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <SettingsIcon/>
+                    </IconButton>
+                </div>
+            </div>
+            <div className="emailList__sections">
+
+            </div>
         </div>
     )
 }
