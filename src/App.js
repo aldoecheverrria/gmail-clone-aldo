7083978fa27d8c8e40154c14a54bcccbc156aad6
aldoecheverria <aldoecheverria@yahoo.com.mx>
2021-06-18 20:11:26 -0500

    new components, and added React Router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,36 @@ import React from 'react';
 import './App.css';
 import Header from './Header'
 import Sidebar from './Sidebar';
+import Mail from './Mail'
+import EmailList from './EmailList';
+import {
+
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <div className="app">
       <Header/>
-      <Sidebar/>
+      <div className="app__body">
+        <Sidebar/>
+        <Switch>
+          <Route path="/mail">
+            <Mail/>
+          </Route>
+          <Route path="/">
+            <EmailList/>
+          </Route>
+        </Switch>
+      </div>
+      
       <h1>Este será el clon de Gmail</h1>
     </div>
+    </Router>
   );
 }
 
