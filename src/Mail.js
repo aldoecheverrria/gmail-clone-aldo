ec8f60e62c1a1ddc5af8c26e65f113a6110c4940
aldoecheverria <aldoecheverria@yahoo.com.mx>
2021-06-23 20:16:45 -0500

    starting the styling of the mail component

--- src/Mail.js
+++ src/Mail.js
@@ -9,6 +9,9 @@ import WatchLaterIcon from "@material-ui/icons/WatchLater";
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import LabelImportantIcon from "@material-ui/icons/LabelImportant";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import UnfoldMoreIcon from "@material-ui/icons/UnfoldMore";
+import PrintIcon from "@material-ui/icons/Print";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import { IconButton } from '@material-ui/core';
 import {useHistory} from "react-router-dom";
 
@@ -60,7 +63,15 @@ function Mail() {
 
                 </div>
                 <div className="mail__toolsRight">
-
+                    <IconButton>
+                        <UnfoldMoreIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <PrintIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <ExitToAppIcon/>
+                    </IconButton>
                 </div>
 
             </div>
